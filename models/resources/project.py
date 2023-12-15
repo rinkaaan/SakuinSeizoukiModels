938507a354bea4b97e270bc4a884dc27ff38a791
Lincoln Nguyen <137611486+rinkaaan@users.noreply.github.com>
2023-12-14 17:19:15 -0800

    Init

---
+++ models/resources/project.py
@@ -0,0 +1,24 @@
+import uuid
+
+from sqlalchemy import Column, String, DateTime, ColumnElement
+
+from models.base import Base
+from nguylinc_python_utils.sqlalchemy import BaseExtended
+
+
+class Project(Base, BaseExtended):
+    __tablename__ = "projects"
+    id = Column(String(36), primary_key=True, index=True, default=uuid.uuid4)
+    created_at: ColumnElement = Column(DateTime(), index=True)
+    updated_at: ColumnElement = Column(DateTime(), index=True)
+    name = Column(String(100), index=True, unique=True)
+    editable_fields = ["name"]
+
+    def get_pdf_location(self):
+        return f"{self.name}/book.pdf"
+
+    def get_word_list_location(self):
+        return f"{self.name}/word_list.xlsx"
+
+    def get_book_index_location(self):
+        return f"{self.name}/book_index.xlsx"
---
+++ scripts/install_models.sh
@@ -0,0 +1,7 @@
+WORKPLACE="$HOME/workplace/SakuinSeizouki"
+
+(
+  cd "$WORKPLACE/SakuinSeizoukiModels"
+  pip install .
+  rm -rf build
+)
